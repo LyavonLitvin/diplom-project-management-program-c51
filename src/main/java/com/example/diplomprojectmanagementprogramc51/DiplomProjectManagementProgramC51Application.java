4a342c9498c4e78098f11411e06870ab4a5e76f8
LyavonLitvin <leonid.tihonchuk@gmail.com>
2022-04-28 19:08:46 +0300

    Initial commit

---
+++ src/main/java/com/example/diplomprojectmanagementprogramc51/DiplomProjectManagementProgramC51Application.java
@@ -0,0 +1,13 @@
+package com.example.diplomprojectmanagementprogramc51;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomProjectManagementProgramC51Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiplomProjectManagementProgramC51Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/diplomprojectmanagementprogramc51/DiplomProjectManagementProgramC51ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.diplomprojectmanagementprogramc51;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomProjectManagementProgramC51ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
