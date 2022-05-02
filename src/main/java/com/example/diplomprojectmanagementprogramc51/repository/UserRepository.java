2e62b9f361abac55e3a60a814ad5d35fadd43e75
LyavonLitvin <leonid.tihonchuk@gmail.com>
2022-05-02 15:54:01 +0300

    2

--- src/main/java/com/example/diplomprojectmanagementprogramc51/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.diplomprojectmanagementprogramc51.repository;
-
-import com.example.diplomprojectmanagementprogramc51.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-	User findByUsername(String username);
-}
