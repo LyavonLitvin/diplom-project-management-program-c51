853acd7327668ff1c3789456e54ffaf7f597f6d3
LyavonLitvin <leonid.tihonchuk@gmail.com>
2022-05-02 10:53:06 +0300

    1

---
+++ src/main/java/com/example/diplomprojectmanagementprogramc51/controller/CalcController.java
@@ -0,0 +1,24 @@
+package com.example.diplomprojectmanagementprogramc51.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/calc")
+public class CalcController {
+
+	@GetMapping
+	public String calc() {
+		return "calc";
+	}
+
+	@PostMapping
+	public String calc(double a, double b, String op, Model model) {
+		double result = a + b;
+		model.addAttribute("result", result);
+		return "calc";
+	}
+}
